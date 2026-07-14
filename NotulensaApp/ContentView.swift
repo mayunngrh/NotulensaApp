4eec3ee79498fcf6836c2a95c61ec682d4a87997
Mayun Suryatama <mayunngrh@gmail.com>
2026-07-15 01:01:19 +0800

    Initial Commit

---
+++ NotulensaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotulensaApp
+//
+//  Created by Mayun Suryatama on 15/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotulensaApp/NotulensaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotulensaAppApp.swift
+//  NotulensaApp
+//
+//  Created by Mayun Suryatama on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct NotulensaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
